ef833981cb3529f1e80099c43fd0d41ca0abe523
Nithin <nithinjosetom@gmail.com>
2016-12-08 14:00:36 +0530

    Added hide completed tasks checkbox

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,8 +8,24 @@ import Task from './Task.jsx';
  
 // App component - represents the whole app
 class App extends Component {
+  constructor(props) {
+    super(props);
+ 
+    this.state = {
+      hideCompleted: false,
+    };
+  }
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: !this.state.hideCompleted,
+    });
+  }
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -32,6 +48,17 @@ class App extends Component {
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+           <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted.bind(this)}
+            />
+            Hide Completed Tasks
+          </label>
+
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
             <input
               type="text"
