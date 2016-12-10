e59834df264309dd54e46d62fb35638edb66b0b2
Nithin <nithinjosetom@gmail.com>
2016-12-10 18:46:45 +0530

    Removed insecure module; modified to make actions more secure

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,3 +1,41 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
  
 export const Tasks = new Mongo.Collection('tasks');
+ 
+Meteor.methods({
+  'tasks.insert'(text) {
+    check(text, String);
+ 
+    // Make sure the user is logged in before inserting a task
+    if (! this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+ 
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+      owner: this.userId,
+      username: Meteor.users.findOne(this.userId).username,
+    });
+  },
+  'tasks.remove'(taskId) {
+    check(taskId, String);
+
+    if (! this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+ 
+    Tasks.remove(taskId);
+  },
+  'tasks.setChecked'(taskId, setChecked) {
+  	 if (! this.userId) {
+      throw new Meteor.Error('not-authorized');
+    }
+    check(taskId, String);
+    check(setChecked, Boolean);
+ 
+    Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+});
