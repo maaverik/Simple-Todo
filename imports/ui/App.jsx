581dda22691cc6cac6bfb597d7199a1196503944
Nithin <nithinjosetom@gmail.com>
2016-12-14 13:41:47 +0530

    removed autopublish; made tasks optionally private to users;

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -27,9 +27,18 @@ class App extends Component {
     if (this.state.hideCompleted) {
       filteredTasks = filteredTasks.filter(task => !task.checked);
     }
-    return filteredTasks.map((task) => (
-      <Task key={task._id} task={task} />
-    ));
+    return filteredTasks.map((task) => {
+      const currentUserId = this.props.currentUser && this.props.currentUser._id;
+      const showPrivateButton = task.owner === currentUserId;
+ 
+      return (
+        <Task
+          key={task._id}
+          task={task}
+          showPrivateButton={showPrivateButton}
+        />
+      );
+    });
   }
   handleSubmit(event) {
     event.preventDefault();
@@ -87,6 +96,7 @@ App.propTypes = {
 };
  
 export default createContainer(() => {
+  Meteor.subscribe('tasks');
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
     incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
